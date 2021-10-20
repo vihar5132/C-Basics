f146a5372ddbc40e890a45067ed50cb0d7cd4d7c
VIHAR POTHUKUCHI <67669712+vihar5132@users.noreply.github.com>
2021-10-20 12:23:26 +0530

    c programming on cgpa, initial, age   dt:20/10/2021

---
+++ INITIAL CGPA AGE PRGRAMME.c
@@ -0,0 +1,16 @@
+/* CPROGRAMME TO PRINT HELLO WORLD*/
+#include<stdio.h>
+void main()
+{
+    int age;// variable deceleration 
+	char initial;
+	float cgpa;
+	
+age=19; //initialization 
+cgpa=8.09;
+initial='p';
+
+printf("age of the student%d\n",age); //age of the student 
+printf("cgpa of the student %f\n",cgpa);//cgpa of the student 
+printf("initial of the student%c\n",initial);//initial of the student	
+}
