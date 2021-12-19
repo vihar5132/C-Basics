ebf665885f4e612f63a5e3a0a68d94cb209cff25
VIHAR POTHUKUCHI <67669712+vihar5132@users.noreply.github.com>
2021-12-19 06:38:25 +0530

    Add files via upload

---
+++ exe.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+void main()
+{
+	char name[20];
+	printf("ENTER  NAME");
+	scanf("%s",&name);
+	system("cls");
+	printf("\n\n\n");
+	printf("\t		     ''''\n");
+	printf("\t		     ||||\n");
+	printf("\t             :===================;\n");
+	printf("\t	   :==========================;  \n");
+	printf("\t      :====================================;'\n");
+	printf("\t   {*************************************************}\n");
+	printf("\t------------------------------------------------------------\n");
+	printf("May the joy that you have spread in the past come back to you on this day. Wishing you a very happy birthday!\n");
+	printf("\t                                HAPPY BIRTHDAY\n");
+	printf("\t\t                           %s\n\n\n",name);
+	getch();
+	return 0;
+
+}
